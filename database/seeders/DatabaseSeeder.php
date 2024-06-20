2cd66f346ef65e6200775978336b78ad7da76e2a
sanaahamliri <sanahamlirifr01@gmail.com>
2024-06-20 15:47:11 +0100

    listings factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
+        Listing::factory(6)->create();
+
+
 
         User::factory()->create([
             'name' => 'Test User',
