283da01dc3b43bec63ec81d4ef13553bbfde0b85
sanaahamliri <sanahamlirifr01@gmail.com>
2024-06-21 10:44:00 +0100

    fixing user registration

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,13 +26,17 @@
     <nav class="flex justify-between items-center mb-4">
         <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
+            @if (Route::has('register'))
             <li>
-                <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                <a href="{{ route('register') }}" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
+            @endif
+            @if (Route::has('login'))
             <li>
-                <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                <a href="{{ route('login') }}" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
+            @endif
         </ul>
     </nav>
 
