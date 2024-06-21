7807ef810747ea9d42bc4af5db5e25d1949e881c
sanaahamliri <sanahamlirifr01@gmail.com>
2024-06-21 14:39:12 +0100

    remove breeze and fixing authentification and put middlwares

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -43,7 +44,7 @@ public function store(Request $request) {
         $formFields['logo'] = $request->file('logo')->store('logos', 'public');
     }
 
-    // $formFields['user_id'] = auth()->id();
+    $formFields['user_id'] = auth()->id();
 
     Listing::create($formFields);
 
--- routes/web.php
+++ routes/web.php
@@ -10,36 +10,42 @@ use Illuminate\Support\Facades\Route;
 // ******All Listings**********
 Route::get('/', [ListingController::class, 'index']);
 
-Route::get('/listings/create',[ListingController::class, 'create']);
+Route::get('/listings/create',[ListingController::class, 'create'])->middleware('auth');
 
 // store data
-Route::post('/listings',[ListingController::class, 'store']);
+Route::post('/listings',[ListingController::class, 'store'])->middleware('auth');
 
 // edit form
-Route::get('/listings/{listing}/edit',[ListingController::class, 'edit']);
+Route::get('/listings/{listing}/edit',[ListingController::class, 'edit'])->middleware('auth');
 
 
 // edit submit to update
 
-Route::put('/listings/{listing}',[ListingController::class, 'update']);
+Route::put('/listings/{listing}',[ListingController::class, 'update'])->middleware('auth');
 
 // delete listing
-Route::delete('/listings/{listing}',[ListingController::class, 'destroy']);
+Route::delete('/listings/{listing}',[ListingController::class, 'destroy'])->middleware('auth');
 
 
 
 
-Route::get('/listings/{listing}',[ListingController::class, 'show']);
+Route::get('/listings/{listing}',[ListingController::class, 'show'])->middleware('guest');
 
 
-Route::get('/register',[UserController::class, 'create']);
+Route::get('/register',[UserController::class, 'create'])->middleware('guest');
 
 // create new user
 Route::post('/users',[UserController::class, 'store']);
 
 
 // log out users
-Route::post('/logout',[UserController::class, 'logout']);
+Route::post('/logout',[UserController::class, 'logout'])->middleware('auth');
+
+// show login form
+Route::get('/login',[UserController::class, 'login'])->name('login')->middleware('guest');
+
+// login users
+Route::post('users/authenticate',[UserController::class, 'authenticate']);
 
 
 
